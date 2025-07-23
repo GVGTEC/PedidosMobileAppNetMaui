55960a6f253351211475c49a8d73bf0f5095459d
GVGTEC <38598848+GVGTEC@users.noreply.github.com>
2025-07-22 22:47:52 -0300

    22-07-2025-22-47

--- PedidosMobileApp/Models/Pedido.cs
+++ PedidosMobileApp/Models/Pedido.cs
@@ -11,9 +11,14 @@ namespace PedidosMobileApp.Models
         public int Id { get; set; }
         public int Mesa { get; set; }
         public int Comanda { get; set; }
+        public string Nome {get; set; }
+        public string Telefone { get; set; }
         public DateTime DataEmissao { get; set; }
         public int QuantasPessoas { get; set; }
         public int FormaPagamento { get; set; }
+        public double SubTotal { get; set; }
+        public double DescontoAcrescimoPc { get; set; }
+        public double DescontoAcrescimo { get; set; }
         public double Total { get; set; }
         public string Flag { get; set; }
         public int ClienteId { get; set; } //PARA DELIVERY
